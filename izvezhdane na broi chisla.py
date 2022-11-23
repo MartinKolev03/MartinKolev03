336b646d53eeb0dd2e657da64635f801956cb216
Martin Kolev <116222807+MartinKolev03@users.noreply.github.com>
2022-11-23 14:02:56 +0200

    Update izvezhdane na broi chisla.py

--- izvezhdane na broi chisla.py
+++ izvezhdane na broi chisla.py
@@ -1,5 +1,10 @@
 a=int(input("Въведи число"))
+s=0
 for i in range(1,a+1):
+    n=0
     for j in range(0,i):
+        n=n*10+a
         print(a,end="")
-    print()
+    print("+",end="")
+    s+=n
+print("="+str(s))
